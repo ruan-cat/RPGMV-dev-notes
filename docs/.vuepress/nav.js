8a2a823c9d0a170d983ddc364253be72b1338ea2
RuanZhongNan <1219043956@qq.com>
2022-08-21 10:06:40 +0800

    插件开发 文件重构

--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -4,77 +4,10 @@ module.exports = [
     // ariaLabel: 'Language Menu',
     items: [
       { text: '基于mv的JavaScript基础', link: '/rmmv-base-js-course/' },
-      { text: '插件开发' },
-      { text: '拓展技术', link: '/extend-tech/' },
+      { text: '插件开发', link: '/write-rmmv-plug/' },
       { text: '源码解析', link: '/reading-source-code/' },
+      { text: '拓展技术', link: '/extend-tech/' },
       { text: '自查资源', link: '/rmmv-resource/' },
-    ],
-  },
-
-  {
-    text: 'JavaScript',
-    items: [
-      {
-        text: 'Java Script',
-        link: '/JavaScript/',
-      },
-    ],
-  },
-  {
-    text: 'TypeScript',
-    items: [
-      {
-        text: 'Type Script',
-        link: '/TypeScript/',
-      },
-    ],
-  },
-  {
-    text: 'cmd',
-    items: [
-      {
-        text: 'Cmd',
-        link: '/cmd/',
-      },
-    ],
-  },
-  {
-    text: 'vue.js',
-    items: [
-      {
-        text: 'Element Ui',
-        link: '/vue.js/element-ui/',
-      },
-      {
-        text: 'Node Sass',
-        link: '/vue.js/node-sass/',
-      },
-      {
-        text: 'Vue',
-        link: '/vue.js/vue/',
-      },
-      {
-        text: 'Vue Js',
-        link: '/vue.js/',
-      },
-    ],
-  },
-
-  {
-    text: '拓展技术',
-    items: [
-      {
-        text: 'V Scode',
-        link: '/拓展技术/VScode/',
-      },
-      {
-        text: 'Webpack Rmmv',
-        link: '/拓展技术/webpack-rmmv/',
-      },
-      {
-        text: '拓展技术',
-        link: '/拓展技术/',
-      },
     ],
   },
 ];
