5d94531cd9145887e97170c5776a49396b7cdc92
ruan-cat.台式机 <1219043956@qq.com>
2023-02-11 12:47:46 +0800

    🤔 SaveFile: 测试生成的东西；

--- docs/.vuepress/navbar/index.ts
+++ docs/.vuepress/navbar/index.ts
@@ -1,1 +1,13 @@
-export * from './zh.js';
+import { navbar } from 'vuepress-theme-hope';
+
+export const zhNavbar = navbar([
+  {
+    text: '阮喵喵自己的笔记',
+    children: [],
+  },
+  {
+    text: '插件开发',
+    prefix: 'developing-rmmv-plugins/',
+    children: ['infrastructure/'],
+  },
+]);
