51339ad9a7b9f79f28ef6ea6e116eccf5a5294ca
阮喵喵 <1219043956@qq.com>
2023-02-22 17:03:32 +0800

    🤔 save-file: 整理拓展曾经的rmmv开发待办；

--- docs/.vuepress/nav-side-common-conf/index.ts
+++ docs/.vuepress/nav-side-common-conf/index.ts
@@ -1,10 +1,10 @@
 // 侧边栏 和 顶部导航栏的数组形式配置类型
 import {
-  SidebarArrayOptions,
-  NavbarOptions,
-  SidebarGroupItem,
-  SidebarStructureItem,
-  SidebarItem,
+	SidebarArrayOptions,
+	NavbarOptions,
+	SidebarGroupItem,
+	SidebarStructureItem,
+	SidebarItem,
 } from "vuepress-theme-hope";
 
 /** 本地自定义的 共同数组类型 @deprecated 实际使用的时候 类型无法共同使用 */
@@ -12,43 +12,51 @@ type CommonArrayOptions = SidebarArrayOptions | NavbarOptions;
 
 /** 顶部导航栏配置数组 */
 export const navConfList: NavbarOptions = [
-  {
-    text: "插件开发",
-    prefix: "/developing-rmmv-plugins/",
-    children: [
-      "index/",
-      "source-code-infrastructure/",
-      "plugins-infrastructure/",
-      "basic-concept/",
-      "nature/",
-    ],
-  },
+	{
+		text: "插件开发",
+		prefix: "/developing-rmmv-plugins/",
+		children: [
+			"index/",
+			"source-code-infrastructure/",
+			"plugins-infrastructure/",
+			"basic-concept/",
+			"nature/",
+		],
+	},
 
-  {
-    text: "源码解析",
-    prefix: "/understand-source-code/",
-    children: [
-      "菜单的背景图为什么是当前场景截图/",
-      "场景类所需的基本函数/",
-      "地图场景与菜单场景之间的切换/",
-      "窗口速查表/",
-      // 'source-code-infrastructure/',
-      // 'plugins-infrastructure/',
-      // 'basic-concept/',
-      // 'nature/',
-    ],
-  },
+	{
+		text: "源码解析",
+		prefix: "/understand-source-code/",
+		children: [
+			"菜单的背景图为什么是当前场景截图/",
+			"场景类所需的基本函数/",
+			"地图场景与菜单场景之间的切换/",
+			"窗口速查表/",
+			// 'source-code-infrastructure/',
+			// 'plugins-infrastructure/',
+			// 'basic-concept/',
+			// 'nature/',
+		],
+	},
 ];
 
 (navConfList as SidebarGroupItem[]).forEach((elm) => (elm.collapsible = true));
 
 /** 左侧侧边栏配置数组 */
 export const sideConfList: SidebarArrayOptions = [
-  ...navConfList,
-  {
-    text: "阮喵喵自己的笔记",
-    prefix: "ruan-cat-own-notes/",
-    children: "structure",
-    collapsible: true,
-  },
+	...navConfList,
+
+	{
+		text: "自查资源",
+		prefix: "self-checking-resources/",
+		children: "structure",
+		collapsible: true,
+	},
+
+	{
+		text: "阮喵喵自己的笔记",
+		prefix: "ruan-cat-own-notes/",
+		children: "structure",
+		collapsible: true,
+	},
 ];
---
+++ docs/ruan-cat-own-notes/learn-todo/todo.md
@@ -0,0 +1,27 @@
+# 个人学习待办
+
+## 设计模式的学习
+
+继承、组合、聚合。这三种东西怎么实现？有什么优缺点？在什么时候使用最好？
+
+## 设计模式
+
+https://notes.youngkbt.cn/design-pattern
+
+## 什么是微前端技术？
+
+了解关于微前端的技术
+https://qiankun.umijs.org/zh
+
+## 构建便于查询的数据结构？
+
+查询？目前有这几种方式实现可以查询性质的数据结构构建。
+
+1： find 函数直接查询。
+2： 构建查询用的数据结构。
+2.1： Object.assign
+2.2： Object.fromEntries
+
+## screen to gif
+
+一款录制工具。需要检查。看看和 LICEcap 之间的差异？
