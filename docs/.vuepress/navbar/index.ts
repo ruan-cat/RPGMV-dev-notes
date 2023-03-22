21848f0f9bebc6b6b9fa37aa4c9c4bb1d5801add
阮喵喵 <1219043956@qq.com>
2023-03-22 13:44:06 +0800

    🤔 save-file: 一些测试代码段；

--- docs/.vuepress/navbar/index.ts
+++ docs/.vuepress/navbar/index.ts
@@ -1,5 +1,22 @@
-import { navbar } from 'vuepress-theme-hope';
+import { navbar } from "vuepress-theme-hope";
 
-import { navConfList } from '../nav-side-common-conf/index.js';
+import { navConfList } from "../nav-side-common-conf/index.js";
 
 export const zhNavbar = navbar(navConfList);
+
+// 一段测试代码
+// type ReallyExist = {
+// 	enrollEndTime: string;
+// 	enrollStartTime: string;
+// 	enrollSurplusNum: number;
+// 	isEnroll: number;
+// };
+// type LifeLongLearnPlan = ReallyExist & Record<string, unknown>;
+// const item: LifeLongLearnPlan = {
+// 	ferfger: "",
+// 	g545: "",
+// 	enrollEndTime: "",
+// 	enrollStartTime: "",
+// 	enrollSurplusNum: 0,
+// 	isEnroll: 0,
+// };
