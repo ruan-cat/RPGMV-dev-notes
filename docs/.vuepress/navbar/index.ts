ea5db8ffe60b20258678207418cf7c438ff87398
阮喵喵 <1219043956@qq.com>
2023-03-23 15:47:07 +0800

    🧪 test: ts const 类型常量写法；

--- docs/.vuepress/navbar/index.ts
+++ docs/.vuepress/navbar/index.ts
@@ -20,3 +20,20 @@ export const zhNavbar = navbar(navConfList);
 // 	enrollSurplusNum: 0,
 // 	isEnroll: 0,
 // };
+
+// 学习const常量写法
+// /** 用户课程状态 配置类型 固定规定的配置类型
+//  * 用const的方式生成唯一的枚举类型
+//  * 参考资料 https://zhuanlan.zhihu.com/p/437553121
+//  */
+// const userCourseStatusConf = {
+// 	1: "已报名",
+// 	2: "学习中",
+// 	3: "完成学习",
+// } as const;
+// /** 用户课程状态 配置类型 常量枚举类型 */
+// export type UserCourseStatusConf = typeof userCourseStatusConf;
+// /** 用户课程状态 */
+// export type UserCourseStatus = keyof UserCourseStatusConf;
+// /** 用户课程状态 显示文本 */
+// export type UserCourseStatusText = UserCourseStatusConf[UserCourseStatus];
