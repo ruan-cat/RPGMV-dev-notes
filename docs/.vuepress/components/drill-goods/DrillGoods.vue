cb6b3f21d51cd1dc5e2ecb3f46c30dfcdd0e2865
阮喵喵 <1219043956@qq.com>
2023-07-06 18:31:48 +0800

    🐎 ci: 增加配置；

--- docs/.vuepress/components/drill-goods/DrillGoods.vue
+++ docs/.vuepress/components/drill-goods/DrillGoods.vue
@@ -1,2 +1,12 @@
+<script lang="ts" setup>
+// import { ref, reactive, defineProps, withDefaults, computed } from "vue";
+// import { storeToRefs } from "pinia";
+</script>
 
+<template>
+	<section>
+		<h3>你哈</h3>
+	</section>
+</template>
 
+<style lang="scss" scoped></style>
