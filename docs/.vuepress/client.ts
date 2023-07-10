0bd55a37d6fb2417fc8bb2fccf013877981d495a
阮喵喵 <1219043956@qq.com>
2023-07-10 14:00:10 +0800

    ✨ feat: 引入pinia 全局持久化插件；

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -3,10 +3,17 @@ import { defineClientConfig } from "@vuepress/client";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 
+import { createPinia } from "pinia";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+
 import DrillGoods from "./components/drill-goods/DrillGoods.vue";
 
 export default defineClientConfig({
 	enhance: ({ app, router, siteData }) => {
+		const pinia = createPinia();
+		pinia.use(piniaPluginPersistedstate);
+
+		app.use(pinia);
 		app.use(ElementPlus);
 		app.component("DrillGoods", DrillGoods);
 	},
