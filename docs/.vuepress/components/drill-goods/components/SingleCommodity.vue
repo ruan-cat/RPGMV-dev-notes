4cd0d7475818ead0ac208ca703ee1ef7506ed2db
阮喵喵 <1219043956@qq.com>
2023-07-10 16:06:14 +0800

    ✨ feat: 显示iconify图标；

--- docs/.vuepress/components/drill-goods/components/SingleCommodity.vue
+++ docs/.vuepress/components/drill-goods/components/SingleCommodity.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import { Icon } from "@iconify/vue";
 import { storeToRefs } from "pinia";
 
 import { useSingleCommodity } from "../stores/use-single-commodity";
@@ -13,12 +14,13 @@ console.log("  in SingleCommodity ", commodity.value);
 		<!-- style="width: 100%" -->
 		<el-table :data="commodity">
 			<el-table-column prop="name" label="名称" min-width="180" />
+
 			<el-table-column prop="desc" label="描述" width="180" />
 
-			<el-table-column prop="icon" label="标签" />
-
 			<el-table-column prop="icon" label="标签">
-				<template #default="scope"> </template>
+				<template #default="scope">
+					<Icon :icon="scope.row.icon"></Icon>
+				</template>
 			</el-table-column>
 		</el-table>
 	</section>
--- docs/.vuepress/components/drill-goods/stores/use-single-commodity.ts
+++ docs/.vuepress/components/drill-goods/stores/use-single-commodity.ts
@@ -1,7 +1,7 @@
 import { defineStore } from "pinia";
 
 import { ref } from "vue";
-import { merge, cloneDeep } from "lodash";
+import { merge, cloneDeep } from "lodash-es";
 
 import type { Ref } from "vue";
 
