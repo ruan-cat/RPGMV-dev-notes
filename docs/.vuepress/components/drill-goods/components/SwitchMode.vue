380e2c127a5f33ba925d2040aa03378772f8f38d
ruan-cat <1219043956@qq.com>
2023-07-14 00:05:06 +0800

    ✨ feat: 切换模式按钮 实现了icon;

--- docs/.vuepress/components/drill-goods/components/SwitchMode.vue
+++ docs/.vuepress/components/drill-goods/components/SwitchMode.vue
@@ -5,6 +5,9 @@ import { useVModel } from "@vueuse/core";
 import { useMode } from "../hooks/use-mode";
 import type { Mode } from "../hooks/use-mode";
 
+import IconsInfo from "./Icons/IconsInfo.vue";
+import IconsSettings from "./Icons/IconsSettings.vue";
+
 type SwitchModeProps = {
 	mode: Mode;
 };
@@ -17,7 +20,10 @@ const emit = defineEmits(["update:mode"]);
 
 const switcher = useVModel(props, "mode", emit);
 
+/** 激活值 即查看 */
 const activeValue = ref<Mode>("info");
+
+/** 失活值 即编辑 */
 const inactiveValue = ref<Mode>("edit");
 
 // import { storeToRefs } from "pinia";
@@ -29,10 +35,17 @@ const inactiveValue = ref<Mode>("edit");
 
 <template>
 	<section>
+		<!-- TODO: 学习使用 vue-i18n v9 来实现集中管理的文本管理 -->
+		<!-- active-text="查看详情"
+			inactive-text="编辑数据" -->
 		<el-switch
 			v-model="switcher"
+			inline-prompt
+			style="--el-switch-off-color: #13ce66"
 			:active-value="activeValue"
 			:inactive-value="inactiveValue"
+			:active-icon="IconsInfo"
+			:inactive-icon="IconsSettings"
 		>
 			{{ switcher }}
 		</el-switch>
