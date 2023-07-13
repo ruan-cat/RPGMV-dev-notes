bfbef1e1d9df2109167b11054791d9cb06109a2f
ruan-cat <1219043956@qq.com>
2023-07-13 23:28:42 +0800

    🤔 save-file: 准备开发一个简单的双向绑定组件；

--- docs/.vuepress/components/drill-goods/components/SwitchMode.vue
+++ docs/.vuepress/components/drill-goods/components/SwitchMode.vue
@@ -1,10 +1,45 @@
 <script lang="ts" setup>
+import { ref } from "vue";
+
+import { useMode } from "../hooks/use-mode";
+import type { Mode } from "../hooks/use-mode";
+
+const { mode } = useMode();
+
+type SwitchModeProps = {
+	mode: Mode;
+};
+
+const props = withDefaults(defineProps<SwitchModeProps>(), {
+	mode: "info",
+});
+
+const switcher = mode;
+switcher.value = "info";
+
+const activeValue = ref<Mode>("info");
+const inactiveValue = ref<Mode>("edit");
+
 // import { storeToRefs } from "pinia";
+
 // import { useSingleCommodity } from "../stores/use-single-commodity";
 // const { commodity } = storeToRefs(useSingleCommodity());
 // console.log("  in SetMenu ", commodity);
 </script>
 
-<template></template>
+<template>
+	<section>
+		<el-switch
+			v-model="switcher"
+			:active-value="activeValue"
+			:inactive-value="inactiveValue"
+			@change=""
+		>
+		</el-switch>
+	</section>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.SwitchMode-root {
+}
+</style>
