cb43e1d80196d3f6bcf6bcdfbce25421e31afdd6
阮喵喵 <1219043956@qq.com>
2023-07-19 18:02:23 +0800

    🔧 build: pnpm i -D @vuepress/plugin-register-components@next

--- docs/.vuepress/components/css-challenges/flex-box-transition/flex-box-transition.vue
+++ docs/.vuepress/components/css-challenges/flex-box-transition/flex-box-transition.vue
@@ -1,5 +1,13 @@
 <script lang="ts" setup></script>
 
-<template></template>
+<template>
+	<section></section>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.flex-box-transition-root {
+	width: 100%;
+	height: 100%;
+	background-color: rgba($color: gray, $alpha: 0.2);
+}
+</style>
