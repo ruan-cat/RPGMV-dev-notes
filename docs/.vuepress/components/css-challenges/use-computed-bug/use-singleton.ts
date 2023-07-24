7bd7a0a7e0757527d8afdcddcd75ba94d4b14b6a
阮喵喵 <1219043956@qq.com>
2023-07-24 17:22:37 +0800

    ✨ feat: 处理了bug。对组合式api有了深入理解；

--- docs/.vuepress/components/css-challenges/use-computed-bug/use-singleton.ts
+++ docs/.vuepress/components/css-challenges/use-computed-bug/use-singleton.ts
@@ -11,8 +11,13 @@ const singletonData = {
 
 	isAdd: false,
 };
+
 export function useSingleton() {
-	const innerData = ref(cloneDeep(singletonData));
+	// 在组合式api中，每次执行一次本函数 都会生成唯一的一个实例 这会实现mixins混入生成独立的实例。
+	// const innerData = ref(cloneDeep(singletonData));
+	// 为了实现全局单例，不应该实现克隆。
+	// 组合式api相当于每次都执行的一个工厂函数。
+	const innerData = ref(singletonData);
 
 	const isAddComputed = computed(() => innerData.value.isAdd);
 
