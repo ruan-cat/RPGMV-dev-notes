aa1791a557cdc4adba6884e113f174c740099031
阮喵喵 <1219043956@qq.com>
2023-09-21 14:58:17 +0800

    🤔 save-file: 针对惠惠提出的问题 处理never的情况；

--- src/typescript-scripts/huihui/p1.ts
+++ src/typescript-scripts/huihui/p1.ts
@@ -1,13 +1,30 @@
 type FilterType = "contain" | "exclude" | "congruence";
 type FilterTarget = "FullLink" | "Domain";
 interface FilterRule {
+	// good
 	type: FilterType;
 	target: FilterTarget;
+
+	// bad
+	// type: FilterType | string;
+	// target: FilterTarget | string;
+
 	value: string;
 }
-type Prettify<T> = {
-	[K in keyof T]: T[K];
-} & {};
-type MyType = Prettify<keyof FilterRule>;
 
-function
+/** 参考资料 https://segmentfault.com/q/1010000020263396 */
+function handle<T extends keyof FilterRule>(
+	value: FilterRule[T],
+	index: number,
+	key: T
+) {
+	const data: FilterRule[] = [];
+	data[index][key] = value;
+}
+
+// type Prettify<T> = {
+// 	[K in keyof T]: T[K];
+// } & {};
+// type KeyOfFilterRule = Prettify<keyof FilterRule>;
+// type ValueOfFilterRule = Prettify<FilterRule[KeyOfFilterRule]>;
+// type FilterRuleV = FilterRule[KeyOfFilterRule];
