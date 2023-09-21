ab7d42ab02eb02b7e5cb1c709431b2708307d7bc
阮喵喵 <1219043956@qq.com>
2023-09-21 16:10:33 +0800

    🤔 save-file: 目前不报错；

---
+++ src/typescript-scripts/huihui/p3.ts
@@ -0,0 +1,24 @@
+export type Fucker = {
+	name: string;
+	money: number;
+};
+export interface IFucker {
+	name: string;
+	money: number;
+}
+
+// export type ParamsArraySetter<T> = {
+// 	list: Array<T>;
+// 	key: keyof T;
+// 	value: T[keyof T];
+// 	index: number;
+// };
+
+function handle<K extends keyof IFucker>(
+	list: Array<IFucker>,
+	index: number,
+	key: K,
+	value: IFucker[K]
+) {
+	list[index][key] = value;
+}
