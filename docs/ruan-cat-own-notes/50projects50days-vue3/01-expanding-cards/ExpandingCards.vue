e1e3bd7cbb13e27e8f099b5d8bee18e42afcc28e
阮喵喵 <1219043956@qq.com>
2023-11-06 11:05:24 +0800

    🤔 save-file: 正在编写 导入组件；

--- docs/ruan-cat-own-notes/50projects50days-vue3/01-expanding-cards/ExpandingCards.vue
+++ docs/ruan-cat-own-notes/50projects50days-vue3/01-expanding-cards/ExpandingCards.vue
@@ -1,14 +1,28 @@
 <script lang="ts" setup>
 import { ref, computed, watch } from "vue";
+
+type Item = {
+	image: string;
+};
+
+const items = ref<Item[]>([{}]);
 </script>
 
 <template>
 	<section class="ExpandingCards-root">
 		<h1>你好 这是 ExpandingCards</h1>
+
+		<section class="container">
+			<section class="item"></section>
+		</section>
 	</section>
 </template>
 
 <style lang="scss" scoped>
 .ExpandingCards-root {
+	.container {
+		.item {
+		}
+	}
 }
 </style>
