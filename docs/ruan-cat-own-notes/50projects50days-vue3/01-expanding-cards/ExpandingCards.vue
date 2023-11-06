e085136d2564f22d10e4e2e5b179237f5657b70d
阮喵喵 <1219043956@qq.com>
2023-11-06 14:07:53 +0800

    🤔 save-file: 正在开发；

--- docs/ruan-cat-own-notes/50projects50days-vue3/01-expanding-cards/ExpandingCards.vue
+++ docs/ruan-cat-own-notes/50projects50days-vue3/01-expanding-cards/ExpandingCards.vue
@@ -1,8 +1,14 @@
 <script lang="ts" setup>
 import { ref, computed, watch } from "vue";
+import { v4 as uuidv4 } from "uuid";
 
+// import {  } from "axios";
+
+/** 遍历的图片项 */
 type Item = {
 	image: string;
+
+	id: string;
 };
 
 /**
@@ -10,45 +16,73 @@ type Item = {
  * @description
  *
  * @see https://www.laoluowl.cn/archives/35
+ * @see https://blog.csdn.net/SectSnow/article/details/115835711
+ * @ses https://www.dmoe.cc/
  */
 const imageApi = [
 	// 樱花随机二次元图片API
-	"https://www.dmoe.cc/random.php",
+	// "https://www.dmoe.cc/random.php",
+	"https://www.dmoe.cc/random.php?return=json",
 
 	// 随机二次元图片API接口
 	"https://api.vvhan.com/api/acgimg",
 ];
 
 const image = computed(
-	() => imageApi[Math.floor(Math.random() * imageApi.length)]
+	// () => imageApi[Math.floor(Math.random() * imageApi.length)]
+	() => imageApi[0]
 );
 
 const items = ref<Item[]>([]);
 
+const itemNumber = 5;
+
 function initItems() {
-	items.value = [
-		{
+	items.value = new Array(itemNumber).fill(1).map((elm) => {
+		return {
 			image: image.value,
-		},
-	];
+			id: uuidv4(),
+		};
+	});
 }
 initItems();
 </script>
 
 <template>
 	<section class="ExpandingCards-root">
-		<h1>你好 这是 ExpandingCards</h1>
-
 		<section class="container">
-			<section class="item" @click="" v-for="item in items"></section>
+			<section
+				class="item"
+				@click=""
+				v-for="item in items"
+				:key="item.id"
+				:style="{ backgroundImage: item.image }"
+			></section>
 		</section>
 	</section>
 </template>
 
 <style lang="scss" scoped>
 .ExpandingCards-root {
+	// 默认占据一半可视高度
+	height: 50vh;
+	width: 100%;
+
 	.container {
+		height: 100%;
+		width: 100%;
+
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: row;
+
 		.item {
+			flex: 1 0 auto;
+			height: 100%;
+
+			background-size: 100% 100%;
+			background-repeat: no-repeat;
 		}
 	}
 }
