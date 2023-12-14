209b140a0a1b4c467aa20734119a59e062e01254
ruan-cat <1219043956@qq.com>
2023-12-14 14:17:15 +0800

    ✨ feat: 初步实现excel文件的导入和识别；

--- docs/ruan-cat-own-notes/js-xlsx/components/load-excel/LoadExcel.vue
+++ docs/ruan-cat-own-notes/js-xlsx/components/load-excel/LoadExcel.vue
@@ -1,20 +1,15 @@
 <script lang="ts" setup>
 import { ref, computed, watch } from "vue";
 
-import { ElAlert, ElButton } from "element-plus";
+import { ElAlert, ElButton, ElUpload } from "element-plus";
 
 import * as XLSX from "xlsx";
 
+type TableData = unknown[];
+
 const title = ref("你好 这是临时使用的文件导入工具");
 
-const tableData = ref([]);
-
-async function beforeUpload(file) {
-	const result = await readXLSX(file);
-	console.log(result);
-	tableData.value = result;
-	return false;
-}
+const tableData = ref<TableData>([]);
 
 function readXLSX(file) {
 	return new Promise((resolve, reject) => {
@@ -22,13 +17,26 @@ function readXLSX(file) {
 
 		reader.readAsBinaryString(file);
 		reader.onload = (evt) => {
-			const data = evt.target.result;
+			const data = evt?.target?.result;
+
 			const wb = XLSX.read(data, { type: "binary" });
-			const ws = workbook.Sheets[workbook.SheetNames[0]];
+
+			// const ws = workbook.Sheets[workbook.SheetNames[0]];
+			const ws = wb.Sheets[wb.SheetNames[0]];
 			const jsonData = XLSX.utils.sheet_to_json(ws);
+
+			console.warn(` 检查导入的数据？ `, jsonData);
+
 			resolve(jsonData);
 		};
 	});
+}
+
+async function beforeUpload(file) {
+	const result = await readXLSX(file);
+	console.log(result);
+	tableData.value = result as TableData;
+	return false;
 }
 </script>
 
@@ -42,7 +50,19 @@ function readXLSX(file) {
 			:closable="false"
 		></el-alert>
 
-		<el-button type="primary" size="large" @click=""> 导入文件 </el-button>
+		<!-- <el-button type="primary" size="large" @click=""> 导入文件 </el-button> -->
+
+		<el-upload
+			ref="upload"
+			:auto-upload="true"
+			accept=".xls,.xlsx"
+			:before-upload="beforeUpload"
+			:show-file-list="false"
+		>
+			<template #trigger>
+				<el-button type="primary">选择文件</el-button>
+			</template>
+		</el-upload>
 	</section>
 </template>
 
