294209882159977314a76905985e1f735c1fd527
ruan-cat <1219043956@qq.com>
2023-12-26 11:34:01 +0800

    ✨ feat: 新建文件 准备实现特定规则的数据读取功能；

---
+++ docs/ruan-cat-own-notes/js-xlsx/components/load-excel/LoadExcel-2.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+import { ref, computed, watch, defineOptions } from "vue";
+</script>
+
+<template>
+	<section class="LoadExcel-2-root"></section>
+</template>
+
+<style lang="scss" scoped>
+.LoadExcel-2-root {
+}
+</style>
