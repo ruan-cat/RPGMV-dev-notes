2dae134eb63b4a247b87e6811688c9021947dc0d
ruan-cat <1219043956@qq.com>
2024-01-09 20:57:55 +0800

    ✨ feat: 使用vitest实现简单的函数测试；

--- src/typescript-scripts/moment-test/isUserRecentGraduate.ts
+++ src/typescript-scripts/moment-test/isUserRecentGraduate.ts
@@ -1,12 +1,11 @@
 import moment from "moment";
 
-const schoolEnd = "2021-07-01";
-
 /**
  * 当前用户是否为`应届毕业生`？
+ * @param { string } schoolEnd 毕业时间
  * @description
  * 当前时间减去毕业时间少于6个月视为应届毕业生
  */
-function isUserRecentGraduate() {
+export function isUserRecentGraduate(schoolEnd: string) {
 	return moment().diff(moment(schoolEnd), "months") <= 6;
 }
