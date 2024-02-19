0cd85b4a1e96d49e403b69093c84e0c23ad0ab4b
ruan-cat <1219043956@qq.com>
2024-02-19 23:18:38 +0800

    ⚙️ config: 根据提示更新配置；

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -56,6 +56,11 @@ export default defineUserConfig({
 		),
 	},
 
+	bundler: viteBundler({
+		viteOptions: {},
+		vuePluginOptions: {},
+	}),
+
 	/**
 	 * 尝试实现element-plus的类型生成，并导入。而不是单纯的组件导入和注册。
 	 * 组件全局注册，已经由vuepress另外实现了。
@@ -119,61 +124,6 @@ export default defineUserConfig({
 		// FIXME: 高版本配置 导致bug
 		// 直接导入项目根目录下的配置文件 这样效率更高
 		// typedocPlugin(typedocConf),
-
-		// 目前有效 缺点是国内使用`algolia`速度稍慢
-		docsearchPlugin({
-			indexName: "ruan-cat",
-			appId: "26AGWKLS5F",
-			// 这是邮件提供的
-			apiKey: "56468a67aa27242ac177690073ba0e15",
-			// 这是爬虫提供的
-			// apiKey: 'c0121d37470f12e725a3cf2b45dda7e7',
-			// 这是用 Search API Key 重新生成的
-			// apiKey: 'd96c038fd76a2f91c146152fbf24a1de',
-			locales: {
-				"/": {
-					placeholder: "搜索文档",
-					translations: {
-						button: {
-							buttonText: "搜索文档",
-							buttonAriaLabel: "搜索文档",
-						},
-						modal: {
-							searchBox: {
-								resetButtonTitle: "清除查询条件",
-								resetButtonAriaLabel: "清除查询条件",
-								cancelButtonText: "取消",
-								cancelButtonAriaLabel: "取消",
-							},
-							startScreen: {
-								recentSearchesTitle: "搜索历史",
-								noRecentSearchesText: "没有搜索历史",
-								saveRecentSearchButtonTitle: "保存至搜索历史",
-								removeRecentSearchButtonTitle: "从搜索历史中移除",
-								favoriteSearchesTitle: "收藏",
-								removeFavoriteSearchButtonTitle: "从收藏中移除",
-							},
-							errorScreen: {
-								titleText: "无法获取结果",
-								helpText: "你可能需要检查你的网络连接",
-							},
-							footer: {
-								selectText: "选择",
-								navigateText: "切换",
-								closeText: "关闭",
-								searchByText: "搜索提供者",
-							},
-							noResultsScreen: {
-								noResultsText: "无法找到相关结果",
-								suggestedQueryText: "你可以尝试查询",
-								reportMissingResultsText: "你认为该查询应该有结果？",
-								reportMissingResultsLinkText: "点击反馈",
-							},
-						},
-					},
-				},
-			},
-		}),
 
 		// componentsPlugin({
 		// 	components: ["BiliBili"],
--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -151,7 +151,7 @@ export default hopeTheme({
 			align: true,
 			alert: true,
 			attrs: true, // 使用特殊标记为 Markdown 元素添加属性
-			container: true,
+			hint: true,
 			gfm: true,
 			imgLazyload: true,
 			imgSize: true,
@@ -171,6 +171,61 @@ export default hopeTheme({
 		 */
 		components: {
 			components: ["BiliBili", "FontIcon", "Badge"],
+		},
+
+		// 目前有效 缺点是国内使用`algolia`速度稍慢
+		docsearch: {
+			indexName: "ruan-cat",
+			appId: "26AGWKLS5F",
+			// 这是邮件提供的
+			apiKey: "56468a67aa27242ac177690073ba0e15",
+			// 这是爬虫提供的
+			// apiKey: 'c0121d37470f12e725a3cf2b45dda7e7',
+			// 这是用 Search API Key 重新生成的
+			// apiKey: 'd96c038fd76a2f91c146152fbf24a1de',
+			locales: {
+				"/": {
+					placeholder: "搜索文档",
+					translations: {
+						button: {
+							buttonText: "搜索文档",
+							buttonAriaLabel: "搜索文档",
+						},
+						modal: {
+							searchBox: {
+								resetButtonTitle: "清除查询条件",
+								resetButtonAriaLabel: "清除查询条件",
+								cancelButtonText: "取消",
+								cancelButtonAriaLabel: "取消",
+							},
+							startScreen: {
+								recentSearchesTitle: "搜索历史",
+								noRecentSearchesText: "没有搜索历史",
+								saveRecentSearchButtonTitle: "保存至搜索历史",
+								removeRecentSearchButtonTitle: "从搜索历史中移除",
+								favoriteSearchesTitle: "收藏",
+								removeFavoriteSearchButtonTitle: "从收藏中移除",
+							},
+							errorScreen: {
+								titleText: "无法获取结果",
+								helpText: "你可能需要检查你的网络连接",
+							},
+							footer: {
+								selectText: "选择",
+								navigateText: "切换",
+								closeText: "关闭",
+								searchByText: "搜索提供者",
+							},
+							noResultsScreen: {
+								noResultsText: "无法找到相关结果",
+								suggestedQueryText: "你可以尝试查询",
+								reportMissingResultsText: "你认为该查询应该有结果？",
+								reportMissingResultsLinkText: "点击反馈",
+							},
+						},
+					},
+				},
+			},
 		},
 
 		// uncomment these if you want a PWA
