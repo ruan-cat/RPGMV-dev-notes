3d8b260cfd2860d7b835513583fdd35b770309f3
ruan-cat <1219043956@qq.com>
2024-03-11 12:37:08 +0800

    ⚙️ config: commitlint.config.ts

---
+++ commitlint.config.ts
@@ -0,0 +1,32 @@
+/**
+ * @see https://juejin.cn/post/6983191125242675230
+ */
+export default {
+	extends: ["@commitlint/config-conventional"],
+	rules: {
+		"type-enum": [
+			2,
+			"always",
+			[
+				"build",
+				"ci",
+				"perf",
+				"feat",
+				"fix",
+				"refactor",
+				"docs",
+				"chore",
+				"style",
+				"revert",
+				"test",
+			],
+		],
+		"type-case": [0],
+		"type-empty": [0],
+		"scope-empty": [0],
+		"scope-case": [0],
+		"subject-full-stop": [0],
+		"subject-case": [0, "never"],
+		"header-max-length": [0, "always", 72],
+	},
+};
