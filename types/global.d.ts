51e923d28e0caa762db08a7de5bd0b1a1ffc42c7
ruan-cat <1219043956@qq.com>
2024-05-25 23:18:27 +0800

    🤔 save-file: 尝试处理vue类型声明配置；

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,6 +1,13 @@
 // 参考资料 https://blog.csdn.net/qq_37718797/article/details/120536817
+// declare module "*.vue" {
+// 	import { defineComponent } from "vue";
+// 	const Component: ReturnType<typeof defineComponent>;
+// 	export default Component;
+// }
+
+// 从vben项目内抄的 尝试实现对vue文件的类型推断
 declare module "*.vue" {
-	import { defineComponent } from "vue";
-	const Component: ReturnType<typeof defineComponent>;
+	import { DefineComponent } from "vue";
+	const Component: DefineComponent<{}, {}, any>;
 	export default Component;
 }
