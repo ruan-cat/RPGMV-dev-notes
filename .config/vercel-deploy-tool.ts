7c7e4379d6cd24a353ab01a428fddd2d919243ac
ruan-cat <1219043956@qq.com>
2024-09-20 15:08:10 +0800

    ⚙️ config: 配置部署命令；

---
+++ .config/vercel-deploy-tool.ts
@@ -0,0 +1,30 @@
+import { type Config } from "@ruan-cat/vercel-deploy-tool/src/config.ts";
+
+const config: Config = {
+	vercelProjetName: "rpgmv-dev-notes-betd",
+	vercelOrgId: "team_cUeGw4TtOCLp0bbuH8kA7BYH",
+	vercelProjectId: "prj_uyMHnLoqFJAOpeBVlHZochteDZdB",
+	vercelToken: "",
+
+	deployTargets: [
+		// 笔记项目
+		{
+			type: "userCommands",
+			outputDirectory: "docs/.vuepress/dist/**/*",
+			targetCWD: "./docs/ruan-cat-notes",
+			url: ["notes.ruan-cat.com", "ruan-cat-notes.ruan-cat.com", "ruan-cat-notes.ruancat6312.top"],
+			userCommands: ["pnpm -C=./docs/ruan-cat-notes build:docs"],
+		},
+
+		// rmmv笔记项目
+		{
+			type: "userCommands",
+			outputDirectory: "docs/.vuepress/dist/**/*",
+			targetCWD: "./docs/rpgmv-dev-notes",
+			url: ["rpgmv-dev-notes.ruancat6312.top", "rpgmv-dev-notes.ruan-cat.com"],
+			userCommands: ["pnpm -C=./docs/rpgmv-dev-notes build:docs"],
+		},
+	],
+};
+
+export default config;
---
+++ bin/vercel-deploy-tool.ts
@@ -0,0 +1,2 @@
+// 执行部署任务
+import "@ruan-cat/vercel-deploy-tool/src/index.ts";
