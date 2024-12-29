99b69278e4ce939a75474e45f9c1fb4b51d7ed02
ruan-cat <1219043956@qq.com>
2024-12-29 20:15:38 +0800

    🤔 save-file(notes): 前端选择用户电脑的文件夹

---
+++ docs/ruan-cat-notes/docs/demo/004-webkitdirectory.md
@@ -0,0 +1,10 @@
+<script setup >
+import WebkitdirectoryDemo from "@docs/demo/code/004-webkitdirectory-demo.vue";
+</script>
+
+# 前端选择用户电脑的文件夹
+
+- https://blog.csdn.net/yxs646512415/article/details/109097588
+- https://developer.mozilla.org/zh-CN/docs/Web/API/HTMLInputElement/webkitdirectory
+
+<WebkitdirectoryDemo />
---
+++ docs/ruan-cat-notes/docs/demo/code/004-webkitdirectory-demo.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import { ref, computed, watch } from "vue";
+
+import { ElInput } from "element-plus";
+
+const inputValue = ref();
+
+function change(event) {
+	console.log(" ?  ", event);
+	// let file = event.target.files;
+	// console.log(file);
+}
+
+// TODO: 待实现
+</script>
+
+<template>
+	<section class="webkitdirectory-demo-root">
+		<!-- <ElInput v-model="inputValue" placeholder="请输入内容" type="file" webkitdirectory @change="change($event)" /> -->
+		<input placeholder="请输入内容" type="file" webkitdirectory @change="change($event)" />
+	</section>
+</template>
+
+<style lang="scss" scoped>
+.webkitdirectory-demo-root {
+}
+</style>
