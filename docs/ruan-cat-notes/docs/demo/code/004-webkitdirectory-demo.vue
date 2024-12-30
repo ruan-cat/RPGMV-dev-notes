19358ed395dcfd52ae9baf8a2e570ac4457ffcd8
ruan-cat <1219043956@qq.com>
2024-12-30 17:45:19 +0800

    🐳 chore(notes): 探索学习 showDirectoryPicker 。

--- docs/ruan-cat-notes/docs/demo/code/004-webkitdirectory-demo.vue
+++ docs/ruan-cat-notes/docs/demo/code/004-webkitdirectory-demo.vue
@@ -11,13 +11,25 @@ function change(event) {
 	// console.log(file);
 }
 
+/**
+ * @see https://juejin.cn/post/7122657335402561550
+ */
+const readDir = async () => {
+	const directoryHandle = await window.showDirectoryPicker();
+	for await (const entry of directoryHandle.values()) {
+		console.log(entry.kind, entry.name);
+	}
+};
+
 // TODO: 待实现
 </script>
 
 <template>
 	<section class="webkitdirectory-demo-root">
+		<ElButton @click="readDir()">readDir</ElButton>
+
 		<!-- <ElInput v-model="inputValue" placeholder="请输入内容" type="file" webkitdirectory @change="change($event)" /> -->
-		<input placeholder="请输入内容" type="file" webkitdirectory @change="change($event)" />
+		<input placeholder="请输入内容" type="file" directory multiple webkitdirectory @change="change($event)" />
 	</section>
 </template>
 
