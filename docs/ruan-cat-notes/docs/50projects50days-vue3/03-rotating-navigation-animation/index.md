046040f6244025d52fb49bee2a6e8f4e11b42247
ruan-cat <1219043956@qq.com>
2025-01-08 06:17:33 +0800

    🎉 init(notes): rotating-navigation-animation

---
+++ docs/ruan-cat-notes/docs/50projects50days-vue3/03-rotating-navigation-animation/index.md
@@ -0,0 +1,13 @@
+<script setup>
+import RotatingNavigationAnimation from "@docs/50projects50days-vue3/03-rotating-navigation-animation/rotating-navigation-animation.vue";
+</script>
+
+# 旋转导航动画
+
+## 自写重构 demo
+
+<RotatingNavigationAnimation />
+
+## 文章教程
+
+- https://juejin.cn/post/7262726113083752503#heading-9
---
+++ docs/ruan-cat-notes/docs/50projects50days-vue3/03-rotating-navigation-animation/rotating-navigation-animation.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup>
+import { ref, computed, watch } from "vue";
+</script>
+
+<template>
+	<section class="rotating-navigation-animation-root">
+		<h2>wefwefg</h2>
+
+		<div class="container">
+			<!-- 旋转的内容... -->
+			<div class="circle-container">
+				<div class="circle">
+					<button id="close">
+						<i class="fas fa-times"></i>
+					</button>
+					<button id="open">
+						<i class="fas fa-bars"></i>
+					</button>
+				</div>
+			</div>
+
+			<div class="content">
+				<!-- 文章内容... -->
+			</div>
+		</div>
+
+		<nav>
+			<ul>
+				<!-- 导航项... -->
+			</ul>
+		</nav>
+	</section>
+</template>
+
+<style lang="scss" scoped>
+.rotating-navigation-animation-root {
+}
+</style>
