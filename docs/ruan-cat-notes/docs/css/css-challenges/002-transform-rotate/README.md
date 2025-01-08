c755a93187aac577cbfc381eff0810b49fbd0f85
ruan-cat <1219043956@qq.com>
2025-01-09 01:06:10 +0800

    🎉 init(notes): 旋转变化动效

---
+++ docs/ruan-cat-notes/docs/css/css-challenges/002-transform-rotate/README.md
@@ -0,0 +1,27 @@
+---
+dir:
+  collapsible: false
+  link: true
+---
+
+<script setup>
+import TransformRotate from "@docs/css/css-challenges/002-transform-rotate/transform-rotate.vue";
+</script>
+
+# 旋转变化动效
+
+## 制作初衷
+
+做 50 天 50 项目时，想着把进度条和旋转动效一起实现出来。
+
+## 动画
+
+<TransformRotate />
+
+## 源码
+
+::: details
+
+@[code vue](./transform-rotate.vue)
+
+:::
---
+++ docs/ruan-cat-notes/docs/css/css-challenges/002-transform-rotate/transform-rotate.vue
@@ -0,0 +1,41 @@
+<script lang="ts" setup>
+import { ref, computed, watch } from "vue";
+
+import { ElButton } from "element-plus";
+</script>
+
+<template>
+	<section class="transform-rotate-root">
+		<section class="circle"></section>
+
+		<section class="btns">
+			<ElButton type="danger"> 减少 </ElButton>
+			<ElButton type="success"> 增加 </ElButton>
+			<ElButton type="primary"> 旋转 </ElButton>
+		</section>
+	</section>
+</template>
+
+<style lang="scss" scoped>
+.transform-rotate-root {
+	width: 100%;
+	height: auto;
+
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
+	gap: 1rem;
+
+	.circle {
+		$size: 20rem;
+		background-color: pink;
+		width: $size;
+		height: $size;
+		border-radius: 50%;
+	}
+
+	.btns {
+	}
+}
+</style>
