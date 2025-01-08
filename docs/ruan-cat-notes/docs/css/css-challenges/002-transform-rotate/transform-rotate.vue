63536c6ab68998f389bfaab08abb38ebc877b9f3
ruan-cat <1219043956@qq.com>
2025-01-09 05:10:35 +0800

    ✨ feat(notes): 实现画圆、

--- docs/ruan-cat-notes/docs/css/css-challenges/002-transform-rotate/transform-rotate.vue
+++ docs/ruan-cat-notes/docs/css/css-challenges/002-transform-rotate/transform-rotate.vue
@@ -2,11 +2,15 @@
 import { ref, computed, watch } from "vue";
 
 import { ElButton } from "element-plus";
+
+const circleItemNum = ref(4);
 </script>
 
 <template>
 	<section class="transform-rotate-root">
-		<section class="circle"></section>
+		<section class="circle">
+			<section class="circle-item" v-for="num in circleItemNum" :key="num" :style="{ '--item-index': num }"></section>
+		</section>
 
 		<section class="btns">
 			<ElButton type="danger"> 减少 </ElButton>
@@ -18,6 +22,17 @@ import { ElButton } from "element-plus";
 
 <style lang="scss" scoped>
 .transform-rotate-root {
+	--circleItemNum: v-bind("circleItemNum");
+	$circleItemNum: 4;
+	// $circleItemNum: var(--circleItemNum);
+
+	// 圆圈直径
+	$circleSize: 20rem;
+	// 圆圈半径
+	$circleRadius: calc($circleSize / 2);
+	// 圆圈每一个项的半径
+	$circle-item-size: 2.5rem;
+
 	width: 100%;
 	height: auto;
 
@@ -28,11 +43,37 @@ import { ElButton } from "element-plus";
 	gap: 1rem;
 
 	.circle {
-		$size: 20rem;
 		background-color: pink;
-		width: $size;
-		height: $size;
+		width: $circleSize;
+		height: $circleSize;
 		border-radius: 50%;
+
+		position: relative;
+
+		.circle-item {
+			width: $circle-item-size;
+			height: $circle-item-size;
+
+			position: absolute;
+			background-color: red;
+
+			// 由ai生成 期望使用循环的方式生成样式
+			// var(--circleItemNum)
+			// @for $i from 1 through $circleItemNum {
+			// 	&:nth-child(#{$i}) {
+			// 		top: 50%;
+			// 		left: 50%;
+			// 		transform: translate(-50%, -50%) rotate(calc(360deg / var(--circleItemNum) * #{$i}));
+			// 	}
+			// }
+
+			--sig-deg: calc(360deg / var(--circleItemNum));
+			--curr-deg: calc(var(--sig-deg) * var(--item-index));
+
+			top: 50%;
+			left: 50%;
+			transform: translate(-50%, -50%) rotate(var(--curr-deg)) translate($circleRadius);
+		}
 	}
 
 	.btns {
