f0a5ea1c0ae21d6a13100305a45127736a6e652c
ruan-cat <1219043956@qq.com>
2025-01-14 11:39:13 +0800

    🗑 del(notes): 删除冗余文件

--- docs/rpgmv-dev-notes/docs/.vuepress/components/css-challenges/flex-box-transition/types/AnimateControlVueProps.d.ts
+++
@@ -1,7 +0,0 @@
-export interface AnimateControlVueProps {
-	/** 是否打开侧边栏？ */
-	isOpenAside: boolean;
-
-	/** 动画时间 */
-	transitionTime: `${number}s`;
-}
--- docs/rpgmv-dev-notes/docs/.vuepress/components/drill-goods/components/Icons/IconsSettings.vue
+++
@@ -1,9 +0,0 @@
-<script lang="ts" setup>
-import { Icon } from "@iconify/vue";
-</script>
-
-<template>
-	<Icon icon="ant-design:setting-twotone"> </Icon>
-</template>
-
-<style lang="scss" scoped></style>
