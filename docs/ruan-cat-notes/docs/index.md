39cc48042b4467677e3c5c5a4e21ae6b76018a24
ruan-cat <1219043956@qq.com>
2025-02-05 01:40:44 +0800

    ✨ feat(notes): 博客首页，实现自动化的文件写入。自动生成出含有项目信息的首页文件。

--- docs/ruan-cat-notes/docs/index.md
+++ docs/ruan-cat-notes/docs/index.md
@@ -9,25 +9,30 @@ title: 阮喵喵的笔记
 heroText: 阮喵喵的笔记
 tagline: 阮喵喵的博客站点。旨在于记录开发经验、练习技术。
 
-# TODO: 写一个接口请求 获取全部包的信息 并以特定格式写入到此处
-projects:
-  - icon: vscode-icons:folder-type-tools
-    name: "utils"
-    desc: 自用的小工具库
-    link: https://www.npmjs.com/package/@ruan-cat/utils
-  - icon: logos:vercel-icon
-    name: "vercel-deploy-tool"
-    desc: 自用的vercel部署工具
-    link: https://www.npmjs.com/package/@ruan-cat/vercel-deploy-tool
-  - icon: logos:vue
-    name: "vuepress-preset-config"
-    desc: 用于给大多数的vuepress项目提供一个预设的配置文件
-    link: https://www.npmjs.com/package/@ruan-cat/vuepress-preset-config
-  - icon: vscode-icons:file-type-vscode
-    name: "generate-code-workspace"
-    desc: 根据 pnpm-workspace.yaml 配置，生成vscode的code-workspace工作区文件
-    link: https://www.npmjs.com/package/@ruan-cat/generate-code-workspace
-
 copyright: false
 footer: MIT Licensed | Copyright © 2022-present 阮喵喵
+
+projects:
+  - name: generate-code-workspace
+    desc: 根据 pnpm-workspace.yaml 配置，生成vscode的code-workspace工作区文件。
+    link: https://npm.im/@ruan-cat/generate-code-workspace
+    icon: vscode-icons:file-type-vscode
+  - name: utils
+    desc: 阮喵喵工具集合。一个纯typescript库，也提供纯js文件。
+    link: https://npm.im/@ruan-cat/utils
+    icon: vscode-icons:folder-type-tools
+  - name: vercel-deploy-tool
+    desc: 阮喵喵自用的vercel部署工具，用于实现复杂项目的部署。
+    link: https://npm.im/@ruan-cat/vercel-deploy-tool
+    icon: logos:vercel-icon
+  - name: vuepress-preset-config
+    desc: 用于给大多数的vuepress项目提供一个预设的配置文件。
+    link: https://npm.im/@ruan-cat/vuepress-preset-config
+    icon: logos:vue
+  - name: commitlint-config
+    desc: 阮喵喵自用的 commitlint.config.cjs 配置。
+    link: https://npm.im/@ruan-cat/commitlint-config
+  - name: domains
+    desc: 为 ruancat6312.top 和 ruan-cat.com 两个域名，提供预设好的常量。旨在于管理全部的域名，便于查询和使用。
+    link: https://npm.im/@ruan-cat/domains
 ---
---
+++ docs/ruan-cat-notes/scripts/make-index-md.ts
@@ -0,0 +1,126 @@
+import * as fs from "node:fs";
+import * as path from "node:path";
+
+import * as yaml from "js-yaml";
+import { type PackageInfo, getRuanCatPkgInfo } from "@ruan-cat/utils/src/ruan-cat-pkg-info";
+
+import type { ThemeBlogHomeProjectOptions } from "vuepress-theme-hope";
+
+interface IconPkgName {
+	name: string;
+	icon: string;
+}
+
+/** 包名和icon名称的配置数组 */
+const iconPkgNames: IconPkgName[] = [
+	{
+		icon: "vscode-icons:folder-type-tools",
+		name: "utils",
+	},
+	{
+		icon: "logos:vercel-icon",
+		name: "vercel-deploy-tool",
+	},
+	{
+		icon: "logos:vue",
+		name: "vuepress-preset-config",
+	},
+	{
+		icon: "vscode-icons:file-type-vscode",
+		name: "generate-code-workspace",
+	},
+];
+
+/**
+ * 将包信息映射为项目选项
+ * @description
+ * - 去除包名中的 `@ruan-cat/` 前缀
+ * - 从 `iconPkgNames` 中查找对应的 icon
+ * - 有icon的就加入，否则不加入
+ */
+function mapPkgInfoToProjectOptions(packageInfos: PackageInfo[]) {
+	return packageInfos.map((pkg) => {
+		const name = pkg.name.replace("@ruan-cat/", "");
+		const iconPkg = iconPkgNames.find((iconPkg) => iconPkg.name === name);
+		const projectOption: ThemeBlogHomeProjectOptions = {
+			name,
+			desc: pkg.description,
+			link: pkg.url,
+		};
+		if (iconPkg) {
+			projectOption.icon = iconPkg.icon;
+		}
+		return projectOption;
+	});
+}
+
+/**
+ * 对项目选项进行排序
+ * @description
+ * - 有icon的排在前面
+ * - 按照名称排序
+ */
+function sortProjectOptions(projectOptions: ThemeBlogHomeProjectOptions[]) {
+	return projectOptions.sort((a, b) => {
+		if (a.icon && !b.icon) return -1;
+		if (!a.icon && b.icon) return 1;
+		return a.name.localeCompare(b.name);
+	});
+}
+
+/**
+ * 生成项目选项的yaml文件内容
+ */
+async function generateProjectOptionsYamlContent() {
+	// 获取包信息
+	const pkgInfo = await getRuanCatPkgInfo();
+	// 将包信息映射为项目选项 并排序
+	const projects = sortProjectOptions(mapPkgInfoToProjectOptions(pkgInfo));
+	// 生成yaml文件内容
+	const projectsContent = yaml.dump({ projects });
+
+	return projectsContent;
+}
+
+async function generateYamlFile(yamlContent: string) {
+	const outputDir = path.join(process.cwd(), "dist");
+	if (!fs.existsSync(outputDir)) {
+		fs.mkdirSync(outputDir);
+	}
+	const outputPath = path.join(outputDir, "ruancat-pkg-info.yaml");
+	fs.writeFileSync(outputPath, yamlContent, "utf8");
+}
+
+/**
+ * 获取基础信息的yaml文件内容
+ * @description
+ * blog-home-base-info.yaml
+ */
+function getBaseInfoYamlContent() {
+	// 从项目根目录读取模板文件 即node笔记项目 而不是单仓根目录
+	const baseInfoPath = path.join(process.cwd(), "./templates/blog-home-base-info.yaml");
+	if (!fs.existsSync(baseInfoPath)) {
+		throw new Error("blog-home-base-info.yaml file not found");
+	}
+	const baseInfoContent = fs.readFileSync(baseInfoPath, "utf8");
+	return baseInfoContent;
+}
+
+/**
+ * 含有yaml的markdown模板
+ */
+function markdownWithYamlTemplate(params: { baseInfoContent: string; projectsContent: string }) {
+	const { baseInfoContent, projectsContent } = params;
+	return `---\n${baseInfoContent}\n${projectsContent}\n---`;
+}
+
+/** 写入md文档 */
+async function writeMdFile() {
+	const baseInfoContent = getBaseInfoYamlContent();
+	const projectsContent = await generateProjectOptionsYamlContent();
+	const mdContent = markdownWithYamlTemplate({ baseInfoContent, projectsContent });
+	const mdPath = path.join(process.cwd(), "./docs/index.md");
+	fs.writeFileSync(mdPath, mdContent, "utf8");
+}
+
+writeMdFile().catch(console.error);
