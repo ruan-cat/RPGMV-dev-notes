fcd2f7110c0210df2479a6ee663a10245a2e2518
ruan-cat <1219043956@qq.com>
2025-06-16 00:58:44 +0800

    🗑 del(rpgmv-dev-notes)!: 移除掉vuepress的配置；使用vitepress的运行命令。

---
+++ docs/rpgmv-dev-notes/docs/.vitepress/theme/index.ts
@@ -0,0 +1,9 @@
+import { defineRuancatPresetTheme } from "@ruan-cat/vitepress-preset-config/theme";
+
+// 增加用户自定义样式
+import "./style.css";
+
+// 增加用户自定义样式
+import "./tools.scss";
+
+export default defineRuancatPresetTheme();
--- docs/rpgmv-dev-notes/docs/.vuepress/config.ts
+++
@@ -1,9 +0,0 @@
-import { defineRuanCatVuepressConfig, hopeTheme } from "@ruan-cat/vuepress-preset-config";
-import { hopeThemeConfig } from "./theme";
-export default defineRuanCatVuepressConfig({
-	title: "阮喵喵的rmmv开发笔记",
-	base: "/",
-	lang: "zh-CN",
-	description: "天才小爱丽丝，冲鸭！",
-	theme: hopeTheme(hopeThemeConfig),
-});
